8248d5a06a6d50fe0761011a3247b54deea27d97
sri chakra sanjay devabathini <srichakrasanjaydevabathini@Sris-MBP.home>
2018-02-05 18:52:07 -0500

    updated users component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: User[]
+  loaded: boolean = true
+  showExtended: boolean = true
   constructor() { }
 
   ngOnInit() {
+    this.users=[
+      // {
+      //   firstName: 'Vamsi',
+      //   lastName: 'Devalla',
+      //   age: 24,
+      //   address: {
+      //     street: '62 rutgers rd',
+      //     city: 'Piscataway',
+      //     state: 'NJ'
+      //   }
+      // },
+      // {
+      //   firstName: 'Sushmitha',
+      //   lastName: 'Patlolla',
+      //   address: {
+      //     street: '62 rutgers rd',
+      //     city: 'Piscataway',
+      //     state: 'NJ'
+      //   }
+      // },
+      // {
+      //   firstName: 'Prakash',
+      //   lastName: 'Mekonda',
+      //   age: 23,
+      //   address: {
+      //     street: '62 rutgers rd',
+      //     city: 'Piscataway',
+      //     state: 'NJ'
+      //   }
+      // }
+    ]
+    // this.addUser(
+    //   {
+    //     firstName: 'Vamshi',
+    //     lastName: 'Girikala',
+    //     age: 23,
+    //     address: {
+    //       street: '62 rutgers rd'
+    //     }
+    //   }
+    // )
   }
-
+  addUser(user:User){
+    this.users.push(user)
+  }
 }
